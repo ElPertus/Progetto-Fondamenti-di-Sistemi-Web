8c8b3893a68259330aae9055ce3e10272bb783ee
ElPertus <108477925+ElPertus@users.noreply.github.com>
2022-07-05 17:49:25 +0200

    Add files via upload

--- Progetto/ricerche.js
+++ Progetto/ricerche.js
@@ -1,7 +1,7 @@
 const vueApp = Vue.createApp({
     data() {
         return {
-            cercati: [{ "nome": "youtube", "ricerche": 1163000000 }, { "nome": "facebook", "ricerche": 1033000000 }, { "nome": "google", "ricerche": 513000000 }, { "nome": "whatsapp web", "ricerche": 490000000 }, { "nome": "never gonna give you up", "ricerche": 18564350 }],
+            cercati: [{ "nome": "youtube", "ricerche": 1163000000 }, { "nome": "facebook", "ricerche": 1033000000 }, { "nome": "google", "ricerche": 513000000 }, { "nome": "whatsapp web", "ricerche": 490000000 }, { "nome": "weather", "ricerche": 400000000 }, { "nome": "never gonna give you up", "ricerche": 18564350 }],
             selected: 0
         }
     },
